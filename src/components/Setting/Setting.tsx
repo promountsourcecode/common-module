d5df5e8aea624a118df824ef4ba1be9eb5b6bfdb
DESKTOP-KU6Q6DD\user <himanshu.promount@gmail.com>
2023-11-21 18:44:21 +0530

    21-11-23

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,354 +1,193 @@
-import React, { useState, Component } from "react";
-import { Checkbox } from "primereact/checkbox";
-import { Dialog } from "primereact/dialog";
-import { Button } from "primereact/button";
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
-import { InputText } from "primereact/inputtext";
-import { Dropdown } from "primereact/dropdown";
-import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faRepeat } from "@fortawesome/free-solid-svg-icons";
-import Translate from "../Translate";
+import { faAnglesRight, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { screenConfigration } from '@promountsourcecode/common_module';
+import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation, NavLink } from 'react-router-dom';
+import React, { useState, useEffect, createContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Tooltip } from 'reactstrap';
+import { LoadingSpinner } from '@promountsourcecode/common_module';
+import { MenuItemTranslate } from 'app/shared/getMenuandmenuItem';
+const Navigation = () => {
+  const [menuData, setMenuData] = useState([]);
+  const [menuItem, setMenuItem] = useState<any>();
+  const [menu, setMenu] = useState<any>();
+  const [coreScreen, setCoreScreen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split('/');
 
-interface ModalInputProps {
-  show: boolean;
-  onSetting: any;
-  columns: any;
-  filter: boolean;
-  gridId: string;
-  gridData: any;
-  onClose: any;
-  onReset: any;
-  menuItemId:any;
-}
+  const items = [
+    { label: menu ? menu.menuName : '', url: menu ? menu.menuLink : '' },
+    { label: menuItem ? menuItem.subMenuName : '', url: menuItem ? menuItem.subMenuLink : '' },
+  ];
 
-class Setting extends Component<ModalInputProps> {
-  tableColumns: any;
+  const home = { icon: 'fa-solid fa-home', url: 'home' };
+  useEffect(() => {
+    console.log("ssss",sessionStorage.getItem('LanguageData'));
+    
+    if (sessionStorage.getItem('LanguageData') === null ) {
+       getMenuDataFromlanguageData();
+     // screenConfigration(0);
+    }
+      const requestUrl = 'api/role-menu-item-mappings/getAllRoleMenuItemMappingByUser/1';
+      setLoading(true);
+      axios.get(requestUrl).then(async(res: any) => {
+        // (await sessionStorage.getItem('LanguageData')) == undefined ? screenConfigration(0) : '';
+       await setMenuData(res.data.data);
+       await sortMenus(menuData);
+       await setLoading(false);
+      });
+  }, []);
 
-  state = {
-    visible: this.props.show,
-    columns: this.props.columns,
-    filter: this.props?.columns[0]?.filterEnable,
-    gridData: this.props.gridData,
-    prop: this.props,
-    pageSize: [
-      { size: "10" },
-      { size: "20" },
-      { size: "50" },
-      { size: "100" },
-      { size: "200" },
-      { size: "500" },
-    ],
-    language: sessionStorage.getItem("Language"),
-    selectedPageSize: {
-      size: this.props?.columns[0]?.gridPageSize,
-    },
+  const getMenuDataFromlanguageData = () => {
+    setLoading(true);
+    axios
+      .get(
+        `/api/screen-configurations/getAllScreenConfigurationsAndScreenControlValidations/${0}/${
+          sessionStorage.getItem('lastSyncTime') ? sessionStorage.getItem('lastSyncTime') : 0
+        }`
+      )
+      .then( res => {
+         sessionStorage.setItem('lastSyncTime', res.data.lastSyncTime);
+         sessionStorage.setItem('LanguageData', JSON.stringify(res.data));
+         screenConfigration(0);
+         setLoading(false);
+      });
   };
 
-  constructor(props) {
-    super(props);
-    if (this.state.gridData.length === 0) {
-      this.tableColumns = this.state.columns;
-      console.log(
-        "this.props?.columns[0]?.gridPageSize",
-        this.state.selectedPageSize,
-        this.state.filter
-      );
+  const checkChild = menu => {
+    let flag: boolean;
+    for (let i = 0; i < menu.subMenuList.length; i++) {
+      if (splitLocation[1].startsWith(menu.subMenuList[i].subMenuLink)) {
+        flag = true;
+        break;
+      } else {
+        flag = false;
+      }
+    }
+    return flag;
+  };
+
+  const sortMenus = data => {
+    data.forEach(e => {
+      if (e.menuName == 'Core-Screens') {
+        setMenuData(e.submenuList);
+      }
+    });
+  };
+
+  const addMenuItemId = async item => {
+    setMenuItem(item);
+    sessionStorage.setItem('currentMenuItem', JSON.stringify(item));
+    sessionStorage.setItem('menuItemId', item.menuItemId);
+    setLoading(true);
+    axios
+      .get(
+        `/api/screen-configurations/getAllScreenConfigurationsAndScreenControlValidations/${item.menuItemId}/${
+          sessionStorage.getItem('lastSyncTime') ? sessionStorage.getItem('lastSyncTime') : 0
+        }`
+      )
+      .then(async res => {
+        setLoading(false);
+        await sessionStorage.setItem('lastSyncTime', res.data.lastSyncTime);
+        await sessionStorage.setItem('LanguageData', JSON.stringify(res.data));
+        await navigate(item.subMenuLink);
+      });
+  };
+
+  function getActiveMethod(menu): boolean {
+    if (splitLocation[1].startsWith(menu.subMenuLink)) {
+      return true;
     } else {
-      this.tableColumns = this.state.gridData;
-      console.log(
-        "this.props?.columns[0]?.gridPageSize",
-        this.state.selectedPageSize,
-        this.state.filter
-      );
+      return false;
     }
   }
-  async getcolumns() {
-    let data: any = [];
 
-    this.props.columns.forEach((column) => {
-      column["gridPageSize"] = this.state.selectedPageSize.size;
-      column["filterEnable"] = this.state.filter;
-    });
-
-    const entity = {
-      gridId: this.state.prop.gridId,
-      gridSettingDetailText: JSON.stringify(this.props.columns),
-      menuItemId: sessionStorage.getItem("menuItemId"),
-      userMasterId: 1,
-      hierarchyLevelId: 352,
-      languageId: 1,
-    };
-
-    data = await axios.put("api/grid-user-settings/saveUpdateData", entity);
-    const dataJson = JSON.parse(data.data.gridSettingDetailText);
-
-    // if(dataJson.length == 0 ){
-    //   this.tableColumns= this.state.columns
-    // }
-    // else{
-    this.tableColumns = dataJson;
-    // }
-  }
-
-  componentDidMount() {
-    // this.getcolumns();
-  }
-
-  toggle = (e) => {
-    e.preventDefault();
-    this.setState({ visible: !this.state.visible });
+  const setMenuObject = menu => {
+    sessionStorage.setItem('currentMenu', JSON.stringify(menu));
   };
 
-  async setSelectedPageSize(e) {
-    console.log("e", e);
-    this.setState({
-      selectedPageSize: e,
-    });
-  }
-
-  checkboxChange = (event, index) => {
-    const data: any = this.tableColumns;
-    data[index].visible = event.checked;
-    this.setState({ columns: data });
-  };
-
-  handleChange() {
-    this.getTabelHeaderData();
-    this.props.onSetting(
-      this.tableColumns,
-      this.state.filter,
-      this.state.selectedPageSize
-    );
-  }
-  handleCancel() {
-    this.setState({
-      visible: false,
-      colums: this.props.columns,
-    });
-    this.props.onClose();
-  }
-  // resetSettings() {
-  // console.log(this.props);
-
-  // this.setState({
-  //   filter: this.props.filter,
-  //   columns: this.props.columns,
-  // });
-
-  // }
-  resetSettings() {
-    this.resetFromServer();
-
-    this.props.onClose();
-  }
-  async resetFromServer() {
-    let id;
-    if (this.state.language === "en") id = 1;
-    else if (this.state.language === "hi") id = 2;
-    else id = 3;
-    const reset = await axios.delete(
-      `/api/grid-user-settings/deleteByUserIdAndHierarchyIdAndGridIdAndMenuItemId?userMasterId=${1}&languageId=${id}&gridId=${
-        this.state.prop.gridId
-      }`
-    );
-  }
-  footerContent = () => {
-    return (
-      <div>
-        <Button
-          label="Apply"
-          icon="pi pi-check"
-          onClick={() => this.handleChange()}
-          autoFocus
-        />
-        <Button label="Reset" onClick={() => this.resetSettings()} />
-        <Button
-          label="Cancel"
-          icon="pi pi-times"
-          onClick={() => {
-            this.handleCancel();
-          }}
-          className="p-button-text"
-        />
-      </div>
-    );
-  };
-
-  async getTabelHeaderData() {
-    let data1: any = [];
-    let id;
-    if (this.state.language === "en") id = 1;
-    else if (this.state.language === "hi") id = 2;
-    else id = 3;
-    this.props.columns.forEach((column) => {
-      column["gridPageSize"] = this.state.selectedPageSize.size;
-      column["filterEnable"] = this.state.filter;
-    });
-
-    const entity = {
-      gridId: String(this.props.gridId),
-      gridSettingDetailText: JSON.stringify(this.state.columns),
-      menuItemId: sessionStorage.getItem("menuItemId"),
-      userMasterId: 1,
-      hierarchyLevelId: 1,
-      languageId: id,
-    };
-
-    data1 = await axios.put("api/grid-user-settings/saveUpdateData", entity, {
-      headers: { menuItemId: this.props.gridId },
-    });
-    const dataJson = JSON.parse(data1.data.gridSettingDetailText);
-  }
-  render() {
-    const cellEditor = (options) => {
-      return textEditor(options);
-    };
-    const textEditor = (options) => {
-      return (
-        <InputText
-          type="text"
-          value={options.value}
-          onChange={(e) => options.editorCallback(e.target.value)}
-        />
-      );
-    };
-    const onCellEditComplete = (e) => {
-      const { rowData, newValue, field, originalEvent: event } = e;
-
-      switch (field) {
-        case "quantity":
-        default:
-          if (newValue.trim().length > 0) rowData[field] = newValue;
-          else event.preventDefault();
-          break;
-      }
-    };
-    const rowReorder = (e) => {
-      // this.tableColumns = null;
-      // this.tableColumns = e.value;
-      if (this.state.gridData.length === 0) {
-        this.setState({ columns: e.value });
-        this.tableColumns = this.state.columns;
-      } else {
-        this.setState({ gridData: e.value });
-        this.tableColumns = this.state.gridData;
-      }
-    };
-    return (
-      <Dialog
-        header="Settings"
-        //footer={this.footerContent}
-        visible={this.state.visible}
-        style={{ width: "80vw" }}
-        onHide={() => {
-          this.handleCancel();
-        }}
-        draggable={false}
-        resizable={false}
-        maximizable
-      >
-        <div>
-          <div className="modal-content">
-            <div className="row">
-              <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-xs-12">
-                {/* <h4> */}
-                <div className="d-flex justify-content-left align-items-left">
-                  <label className="form-label">Filter: </label>
-                  <Checkbox
-                    style={{ marginLeft: "10px" }}
-                    onChange={(event) =>
-                      this.setState({ filter: !this.state.filter })
-                    }
-                    checked={this.state.filter}
-                  ></Checkbox>
-                </div>{" "}
-                {/* </h4> */}
-              </div>
-              <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-xs-12 ">
-                <div className="d-flex justify-content-end align-items-center">
-                  <label className="form-label" style={{ marginRight: "10px" }}>
-                    Page Size:
-                  </label>
-                  <Dropdown
-                    value={this.state.selectedPageSize}
-                    onChange={(e) => this.setSelectedPageSize(e.value)}
-                    options={this.state.pageSize}
-                    optionLabel="size"
-                    placeholder="Select a Page Size"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="tableWrap" style={{ marginTop: "10px" }}>
-              <DataTable
-                value={this.tableColumns}
-                reorderableRows
-                onRowReorder={(e) => rowReorder(e)}
-                responsiveLayout="scroll"
-                rows={this.tableColumns.length}
-                scrollable
+  return (
+    <>
+      <LoadingSpinner visible={loading}></LoadingSpinner>
+      <div className="navigation">
+        <div className="navigation-menu-tab">
+          <ul>
+            <li className="nav-item navigation-toggler mobile-toggler">
+              <a
+                href="#"
+                className="nav-link"
+                data-toggle="tooltip"
+                data-nav-target="nav-link"
+                data-placement="right"
+                title="Show navigation"
               >
-                {/* <Column header="ID" body={props => <div>{props.rowIndex}</div>}></Column> */}
-                <Column rowReorder style={{ width: "3rem" }} />
-                <Column
-                  field="header"
-                  header="Columns"
-                  editor={(options) => cellEditor(options)}
-                  onCellEditComplete={onCellEditComplete}
-                />
-                <Column
-                  field="width"
-                  header="Width"
-                  editor={(options) => cellEditor(options)}
-                  onCellEditComplete={onCellEditComplete}
-                />
-                <Column
-                  header="Display"
-                  body={(data, props) => (
-                    <div>
-                      <Checkbox
-                        onChange={(event) =>
-                          this.checkboxChange(event, props.rowIndex)
-                        }
-                        checked={data.visible}
-                      ></Checkbox>
-                    </div>
-                  )}
-                ></Column>
-                {/* {dynamicColumns} */}
-              </DataTable>
-            </div>
+                <i className="fa-solid fa-bars"></i>
+              </a>
+            </li>
+          </ul>
+          <div className="flex-grow-1">
+            <ul>
+              {!coreScreen &&
+                menuData.map((item, index: Number) => (
+                  <li>
+                    <a
+                      className={checkChild(item) ? 'active' : ''}
+                      href={item.menuLink}
+                      data-toggle="tooltip"
+                      data-nav-target={'#' + item.menuId}
+                      data-placement="right"
+                      title={item.menuName}
+                      onClick={() => setMenuObject(item)}
+                    >
+                      <i className={item.menuIcon}></i>
+                      {/* <Tooltip target={'.' + item.menuIcon} /> */}
+                    </a>
+                  </li>
+                ))}
+            </ul>
           </div>
-          <div className="p-dialog-footer">
-            <Button
-              className="btnStyle btn btn-success"
-              onClick={() => this.handleChange()}
-              autoFocus
-            >
-              <FontAwesomeIcon icon={faCheck} />{" "}
-              <Translate contentKey="home.apply"></Translate>
-            </Button>
-            <Button
-              className="btnStyle btn btn-info"
-              onClick={() => this.resetSettings()}
-            >
-              <FontAwesomeIcon icon={faRepeat} />{" "}
-              <Translate contentKey="home.reset"></Translate>
-            </Button>
-
-            <Button
-              className="btnStyle btn btn-danger"
-              onClick={() => this.handleCancel()}
-            >
-              <FontAwesomeIcon icon="times" />
-              <Translate contentKey="home.close"></Translate>
-            </Button>
+          <div>
+            <ul>
+              <li>
+                <a href="/logout" data-toggle="tooltip" data-placement="right" title="Logout">
+                  <FontAwesomeIcon icon={faRightFromBracket} />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </Dialog>
-    );
-  }
-}
-export default Setting;
+        <div className="navigation-menu-body">
+          <div className="navigation-menu-group">
+            {!coreScreen &&
+              menuData.map(
+                (data, index) =>
+                  data.hasSubMenu && (
+                    <div className={checkChild(data) ? 'open' : ''} id={data.menuId}>
+                      <ul>
+                        <li className="navigation-divider">
+                          <i className={data.menuIcon}></i> <MenuItemTranslate contentKey={data.keyName} />
+                        </li>
+                        {data.subMenuList.map((item, i) => (
+                          <li className="toggleEdit">
+                            <a className={getActiveMethod(item) ? 'active' : ''} onClick={() => addMenuItemId(item)}>
+                              <FontAwesomeIcon icon={faAnglesRight} />
+                              <MenuItemTranslate contentKey={item.keyName} />
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )
+              )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navigation;
