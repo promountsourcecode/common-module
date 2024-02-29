ca6ee3d9d4e9fbfd88d3164a694b7c1d2201ad8d
DESKTOP-KU6Q6DD\user <himanshu.promount@gmail.com>
2024-02-29 19:13:31 +0530

    add constatnt

--- src/components/constants/apiConstant.ts
+++ src/components/constants/apiConstant.ts
@@ -1,4 +1,5 @@
 export const CORE_BASE_URL ='/services/coreweb/';
 export const GATEWAY_BASE_URL  ='/services/gateway/';
 export const QMS_BASE_URL  ='/services/qualitymanagementsystem/';
+export const BUSSINESS_PROCESS_SERVICE  ='/services/businessprocessservice/';
 export const Itegration_service  ='/services/integrationservice/';
