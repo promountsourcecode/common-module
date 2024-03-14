1454324d1f9b368669c01159163644f1a07d3113
Himanshu_Nagar <himanshu.promount@gmail.com>
2024-03-14 19:07:37 +0530

    bread crums in some changes.

--- src/components/BreadCrums/breadCrumbs.tsx
+++ src/components/BreadCrums/breadCrumbs.tsx
@@ -2,7 +2,8 @@ import { BreadCrumb } from 'primereact/breadcrumb';
 import React, { useEffect, useState } from 'react';
 import { MenuItemTranslate } from '../MenuItemTranslate/getMenuandmenuItem';
 import { MenuTranslate } from '../MenuTranslate/menuTranslate';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 const BreadCrumbs = () => {
   const [menuItem, setMenuItem] = useState<any>();
   const [menu, setMenu] = useState<any>();
@@ -33,20 +34,28 @@ const BreadCrumbs = () => {
 
   return (
     <>
-      {/* <h4 style={{ marginRight: '70rem' }} >{menuItem ? <MenuItemTranslate contentKey={menuItem.keyName} /> : ''}</h4>
-      <BreadCrumb className="breadCrumb-header float-right" model={items} home={home} /> */}
       {showbreadCrums && (
         <div className="page-header d-flex justify-content-between">
           <div>
-            {/* {/ <h4>{breadCrums == 'true' ? <BreadCrumbs / > : <Translate contentKey="level.title"></Translate>}</h4> /} */}
             <h4>{menuItem ? <MenuItemTranslate contentKey={menuItem.keyName} /> : ''}</h4>
           </div>
+
           <div><h4><BreadCrumb className="breadCrumb-header float-right" model={items} home={home} /></h4></div>
+
+        </div>
+      )}
+
+      {!showbreadCrums && (
+        <div className="page-header d-flex justify-content-between">
+          <div>
+            <h4>
+              <FontAwesomeIcon icon={faHouse} /> Dashboard{' '}
+            </h4>
+          </div>
         </div>
       )}
 
     </>
-    // <BreadCrumb className="breadCrumb-header" model={items} home={home} />
   )
 
 };
