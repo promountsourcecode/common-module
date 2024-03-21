fbfa8d242fc93a8d3edecad7550fa721473fd1a0
Himanshu_Nagar <himanshu.promount@gmail.com>
2024-03-21 19:58:43 +0530

    Breadcrums issue fixed.

--- src/components/BreadCrums/breadCrumbs.tsx
+++ src/components/BreadCrums/breadCrumbs.tsx
@@ -4,26 +4,26 @@ import { MenuItemTranslate } from '../MenuItemTranslate/getMenuandmenuItem';
 import { MenuTranslate } from '../MenuTranslate/menuTranslate';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
-const BreadCrumbs = () => {
-  const [menuItem, setMenuItem] = useState<any>();
-  const [menu, setMenu] = useState<any>();
-  //   let menu: any = JSON.parse(sessionStorage.getItem('currentMenu' ? 'currentMenu' : ''));
-  //   let menuItem: any = JSON.parse(sessionStorage.getItem('currentMenuItem' ? 'currentMenuItem' : ''));
 
-  useEffect(() => {
-    setMenuItem(JSON.parse(sessionStorage.getItem('currentMenuItem')));
-    setMenu(JSON.parse(sessionStorage.getItem('currentMenu')));
-  }, []);
+const BreadCrumbs = (props: any) => {
+
+  const languageDataLocal = sessionStorage.getItem('Language');
+  const languageDataLocalForMenu = JSON.parse(sessionStorage.getItem('LanguageData'));
+  const currentMenu = useAppSelector(state => state.level.menuData);
+  let menu: any = JSON.parse(sessionStorage.getItem('currentMenu' ? 'currentMenu' : ''));
+  let menuItem: any = JSON.parse(sessionStorage.getItem('currentMenuItem' ? 'currentMenuItem' : ''));
+
+
+  //setIsMandatory(languageDataLocalForMenu['menuLanguageData'][selectLanguage][prop.contentKey]);
   const items: any = [
-    { label: menu ? <MenuTranslate contentKey={menu.keyName} /> : '' },
-    { label: menuItem ? <MenuItemTranslate contentKey={menuItem.keyName} /> : '' },
+    { label: menu ? JSON.parse(sessionStorage.getItem('LanguageData'))['menuLanguageData'][languageDataLocal][menu.keyName]['text'] : '' },
+    { label: menuItem ? JSON.parse(sessionStorage.getItem('LanguageData'))['menuItemLanguageData'][languageDataLocal][menuItem.keyName]['text'] : '' },
   ];
 
-  const home = { icon: 'fa-solid fa-home', url: '' };
 
+  const home = { icon: 'fa-solid fa-home', url: '' };
   const [showbreadCrums, setBreadcrums] = useState<boolean>(false);
   useEffect(() => {
-    //  setLogoId(props.logoId);
     if (sessionStorage.getItem("menuItemId") == "11731") {
       setBreadcrums(false)
     }
@@ -37,11 +37,12 @@ const BreadCrumbs = () => {
       {showbreadCrums && (
         <div className="page-header d-flex justify-content-between">
           <div>
-            <h4>{menuItem ? <MenuItemTranslate contentKey={menuItem.keyName} /> : ''}</h4>
+            <h4>
+              <i className={menu.menuIcon} ></i>
+              {menuItem ? JSON.parse(sessionStorage.getItem('LanguageData'))['menuItemLanguageData'][languageDataLocal][menuItem.keyName]['text'] : ''}
+            </h4>
           </div>
-
           <div><h4><BreadCrumb className="breadCrumb-header float-right" model={items} home={home} /></h4></div>
-
         </div>
       )}
 
@@ -54,10 +55,8 @@ const BreadCrumbs = () => {
           </div>
         </div>
       )}
-
     </>
   )
-
 };
 
 export default BreadCrumbs;
