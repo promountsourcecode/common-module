5f64f0407c5fcba931ec7580a5d456ea41073923
Himanshu_Nagar <himanshu.promount@gmail.com>
2024-04-19 18:38:16 +0530

    Revert "First time login time Translate not working issue fixed."
    This reverts commit 803285e9b11b7e36de80a877f4fc7be1f78fd404.

--- src/components/Translate/Translate.tsx
+++ src/components/Translate/Translate.tsx
@@ -1,58 +1,56 @@
-import { CORE_BASE_URL } from '@promountsourcecode/common_module';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { isFieldMandatory } from "../ValidationMethod";
+import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
-
 export const Translate = (prop) => {
-  const [selectLanguage, setSelectLanguage] = useState(sessionStorage.getItem("Language") == null ? 'en' : sessionStorage.getItem("Language"));
+  const [selectLanguage, setSelectLanguage] = useState(
+    sessionStorage.getItem("Language")
+  );
+  const [languageAPIData, setLanguageAPIData] = useState<any>();
   const [isMandatory, setIsMandatory] = useState<any>([]);
+  const [languageData, setLanguageData] = useState<any>();
   const [lableFlag, setLableFlag] = useState<boolean>(false);
   const [finalValue, setFinalValue] = useState<any>();
-  const [getValue, setGetValue] = useState<any>();
+  const [menuItemId, setMenuItemId] = useState(
+    sessionStorage.getItem("menuItemId")
+  );
+  const navigate = useNavigate();
 
 
   useEffect(() => {
-    if (sessionStorage.getItem("LanguageData") == null) {
-      axios
-        .get(
-          `${CORE_BASE_URL}/api/screen-configurations/getAllScreenConfigurationsAndScreenControlValidations/${0}/${sessionStorage.getItem('lastSyncTime') ? sessionStorage.getItem('lastSyncTime') : 0}`
-        )
-        .then(res => {
-          sessionStorage.setItem('lastSyncTime', res.data.lastSyncTime);
-          sessionStorage.setItem('LanguageData', JSON.stringify(res.data));
-          setGetValue(JSON.stringify(res.data))
-          fetchData()
-          //  setLoading(false);
-        });
-    }
-    else {
-      fetchData();
-    }
+    fetchData();
   }, [""]);
 
   const fetchData = () => {
-    let languageDataLocal = JSON.parse(sessionStorage.getItem("LanguageData")) == null ? JSON.parse(getValue) : JSON.parse(sessionStorage.getItem("LanguageData"));
+    const languageDataLocal = JSON.parse(
+      sessionStorage.getItem("LanguageData")
+    );
+    // if (languageDataLocal == undefined) {
+    //   navigate("/logout");
+    // }
 
     try {
-      if (languageDataLocal) {
+      if(languageDataLocal){
         if (
           languageDataLocal["translations"][selectLanguage][prop.contentKey] !=
           undefined
         )
           setFinalValue(
             languageDataLocal["translations"][selectLanguage][prop.contentKey][
-            "text"
+              "text"
             ]
           );
         setIsMandatory(
           languageDataLocal["translations"][selectLanguage][prop.contentKey]
         );
+        // if (languageDataLocal['translations'][selectLanguage][prop.contentKey]['type'] != undefined) {
         const obj = languageDataLocal["translations"][selectLanguage][
           prop.contentKey
         ]["type"]
           ? languageDataLocal["translations"][selectLanguage][prop.contentKey][
-          "type"
-          ]
+              "type"
+            ]
           : "";
         if (
           obj == "Textarea" ||
@@ -66,37 +64,36 @@ export const Translate = (prop) => {
           setLableFlag(false);
         }
       }
-      } catch (error) {
-        toast.error(error.toString())
-      }
-      // }
-    };
+    } catch (error) {
+      toast.error(error.toString())
+    }
+    // }
+  };
 
-    return (
-      <>
-        {isMandatory != undefined ? <span>{isMandatory.text} </span> : ""}
-        {isMandatory != undefined ? (
-          isMandatory.mandatory === true ? (
-            lableFlag == true ? (
-              <>
-                {" "}
-                <span>:</span>
-                <span className="reqsign">*</span>
-              </>
-            ) : (
-              ""
-            )
-          ) : lableFlag == true ? (
-            <span>:</span>
+  return (
+    <>
+      {isMandatory != undefined ? <span>{isMandatory.text} </span> : ""}
+      {isMandatory != undefined ? (
+        isMandatory.mandatory === true ? (
+          lableFlag == true ? (
+            <>
+              {" "}
+              <span>:</span>
+              <span className="reqsign">*</span>
+            </>
           ) : (
             ""
           )
+        ) : lableFlag == true ? (
+          <span>:</span>
         ) : (
           ""
-        )}
-      </>
-    );
-  };
+        )
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
 
-  export default Translate;
-
+export default Translate;
