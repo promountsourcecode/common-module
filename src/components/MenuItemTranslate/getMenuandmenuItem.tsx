76cce32cea1ca3169211c484e99adc7dafcc0706
Jeel Ghodasara <jeel.bhartitech@gmail.com>
2024-04-25 18:40:34 +0530

    Jeel : Worked on screen configuration remove function

--- src/components/MenuItemTranslate/getMenuandmenuItem.tsx
+++ src/components/MenuItemTranslate/getMenuandmenuItem.tsx
@@ -1,30 +1,50 @@
-import { screenConfigration } from '@promountsourcecode/common_module';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-export const MenuItemTranslate = prop => {
-  const [selectLanguage, setSelectLanguage] = useState<string>(sessionStorage.getItem('Language'));
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+export const MenuItemTranslate = (prop) => {
+  const [selectLanguage, setSelectLanguage] = useState<string>(
+    sessionStorage.getItem("Language")
+  );
   const [isMandatory, setIsMandatory] = useState<any>([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetchData();
-  }, ['']);
+  }, [""]);
 
   const fetchData = () => {
-    const languageDataLocal = JSON.parse(sessionStorage.getItem('LanguageData'));
+    const languageDataLocal = JSON.parse(
+      sessionStorage.getItem("LanguageData")
+    );
     try {
-      if (languageDataLocal && languageDataLocal['menuLanguageData'][selectLanguage][prop.contentKey] != undefined)
-      setIsMandatory(JSON.parse(sessionStorage.getItem('LanguageData'))['menuLanguageData'][selectLanguage][prop.contentKey]);
-    else if (languageDataLocal && languageDataLocal['menuItemLanguageData'][selectLanguage][prop.contentKey] != undefined)
-      setIsMandatory(JSON.parse(sessionStorage.getItem('LanguageData'))['menuItemLanguageData'][selectLanguage][prop.contentKey]);
- 
+      if (
+        languageDataLocal &&
+        languageDataLocal["menuLanguageData"][selectLanguage][
+          prop.contentKey
+        ] != undefined
+      )
+        setIsMandatory(
+          JSON.parse(sessionStorage.getItem("LanguageData"))[
+            "menuLanguageData"
+          ][selectLanguage][prop.contentKey]
+        );
+      else if (
+        languageDataLocal &&
+        languageDataLocal["menuItemLanguageData"][selectLanguage][
+          prop.contentKey
+        ] != undefined
+      )
+        setIsMandatory(
+          JSON.parse(sessionStorage.getItem("LanguageData"))[
+            "menuItemLanguageData"
+          ][selectLanguage][prop.contentKey]
+        );
     } catch (error) {
-      toast.error(error.toString())
+      toast.error(error.toString());
     }
   };
 
-  return <>{isMandatory != undefined ? <span>{isMandatory.text} </span> : ''}</>;
+  return (
+    <>{isMandatory != undefined ? <span>{isMandatory.text} </span> : ""}</>
+  );
 };
 export default MenuItemTranslate;
